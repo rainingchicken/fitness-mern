327c93c96bfc2c6e12107a9c37f198bf87aa8931
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-08-16 23:41:32 -0700

    fixing path abckend

--- backend-fitness/server.js
+++ backend-fitness/server.js
@@ -22,10 +22,12 @@ app.use("/api/workouts", workoutRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend-fitness/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "frontend-fitness", "dist", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => {
